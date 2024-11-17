a680a4eb46ac4cd94b24c1c331433079eda9a18d
Hiroki Kanai <hiroki.kanai0422@gmail.com>
2024-11-17 13:08:36 +0100

    login part, I am not nearly done yet

--- frontEnd/.angular/cache/17.3.11/vite/deps/@angular_platform-browser.js
+++ frontEnd/.angular/cache/17.3.11/vite/deps/@angular_platform-browser.js
@@ -28,7 +28,6 @@ import {
   createApplication,
   disableDebugTools,
   enableDebugTools,
-  getDOM,
   initDomAdapter,
   makeStateKey,
   platformBrowser,
@@ -36,8 +35,11 @@ import {
   provideProtractorTestingSupport,
   withHttpTransferCacheOptions,
   withNoHttpTransferCache
-} from "./chunk-L6VIMCJS.js";
-import "./chunk-5FUJWLSI.js";
+} from "./chunk-X77DZDU5.js";
+import {
+  getDOM
+} from "./chunk-VBIXTFQ5.js";
+import "./chunk-FLSG4Q2H.js";
 export {
   BrowserModule,
   By,
--- frontEnd/.angular/cache/17.3.11/vite/deps/@angular_router.js
+++ frontEnd/.angular/cache/17.3.11/vite/deps/@angular_router.js
@@ -1,13 +1,15 @@
 import {
+  Title
+} from "./chunk-X77DZDU5.js";
+import {
   DOCUMENT,
   HashLocationStrategy,
   LOCATION_INITIALIZED,
   Location,
   LocationStrategy,
   PathLocationStrategy,
-  Title,
   ViewportScroller
-} from "./chunk-L6VIMCJS.js";
+} from "./chunk-VBIXTFQ5.js";
 import {
   APP_BOOTSTRAP_LISTENER,
   APP_INITIALIZER,
@@ -111,7 +113,7 @@ import {
   ɵɵloadQuery,
   ɵɵqueryRefresh,
   ɵɵsanitizeUrlOrResourceUrl
-} from "./chunk-5FUJWLSI.js";
+} from "./chunk-FLSG4Q2H.js";
 
 // node_modules/@angular/router/fesm2022/router.mjs
 var PRIMARY_OUTLET = "primary";
