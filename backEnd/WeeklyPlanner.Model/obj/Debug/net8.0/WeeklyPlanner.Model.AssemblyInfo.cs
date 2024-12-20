f5d8a095cc05453d1650869a819be725618e0219
Ruosa1101 <francesco.dorigo11@gmail.com>
2024-12-20 23:31:05 +0100

    Again

--- backEnd/WeeklyPlanner.Model/obj/Debug/net8.0/WeeklyPlanner.Model.AssemblyInfo.cs
+++ backEnd/WeeklyPlanner.Model/obj/Debug/net8.0/WeeklyPlanner.Model.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeeklyPlanner.Model")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a23638e62d740a070a5cc3f26429aabfd696d912")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a850c656203f8ef00a801f2c300917cc9ae510a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeeklyPlanner.Model")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeeklyPlanner.Model")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
