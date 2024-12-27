d6817ad3993796e520e394afeb0e89faf5647dbf
PhilipTorring <99857867+PhilipTorring@users.noreply.github.com>
2024-12-26 17:41:17 -0800

    Pulling latest

--- backEnd/WeeklyPlanner.API/obj/Debug/net8.0/WeeklyPlanner.API.AssemblyInfo.cs
+++ backEnd/WeeklyPlanner.API/obj/Debug/net8.0/WeeklyPlanner.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeeklyPlanner.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4d7738fb05fd358d0bbef01a720f9e532fefcb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32f1bebc9923fa38a7ebee3aaec311223e982bce")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeeklyPlanner.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeeklyPlanner.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- backEnd/WeeklyPlanner.Model/obj/Debug/net8.0/WeeklyPlanner.Model.AssemblyInfo.cs
+++ backEnd/WeeklyPlanner.Model/obj/Debug/net8.0/WeeklyPlanner.Model.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeeklyPlanner.Model")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4d7738fb05fd358d0bbef01a720f9e532fefcb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32f1bebc9923fa38a7ebee3aaec311223e982bce")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeeklyPlanner.Model")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeeklyPlanner.Model")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
