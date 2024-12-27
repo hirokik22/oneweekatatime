bab25b13babf1d99e2451bbbecd2493184a989ed
PhilipTorring <99857867+PhilipTorring@users.noreply.github.com>
2024-12-26 17:47:57 -0800

    again

--- backEnd/WeeklyPlanner.API/obj/Debug/net8.0/WeeklyPlanner.API.AssemblyInfo.cs
+++ backEnd/WeeklyPlanner.API/obj/Debug/net8.0/WeeklyPlanner.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeeklyPlanner.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+820395c9af0229982be2b2aabd7877493f7d4072")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7733b2012886e10c06af8e9b372b6149490d3be")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeeklyPlanner.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeeklyPlanner.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
